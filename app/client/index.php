a9a0839cde23cb86b540d258ede62738df3e1cb0
btoann <nguyenbaotoan2001@gmail.com>
2020-11-13 22:36:07 +0700

    Update index.php

--- app/client/index.php
+++ app/client/index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-        <link rel="stylesheet" href="view/css/icons/css/fontello.css">
+        <link rel="stylesheet" href="view/icons/css/fontello.css">
     </head>
     <body>
         <i class="icon-duckduckgo"></i>
